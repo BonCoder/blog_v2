9a9d0c3540fef198fa906cd0e9d7aae04d7ab087
BobCoder <bob@bobcoder.cc>
2019-02-19 15:12:28 +0800

    style: 修改图片上传样式

--- resources/views/admin/article/_js.blade.php
+++ resources/views/admin/article/_js.blade.php
@@ -40,6 +40,31 @@
     layui.use(['upload'],function () {
         var upload = layui.upload
 
+        $.("#uploadPic").click(function () {
+            layer.open({
+                type: 2,
+                title: false,
+                closeBtn: 0, //不显示关闭按钮
+                shade: [0],
+                area: ['340px', '215px'],
+                offset: 'rb', //右下角弹出
+                time: 2000, //2秒后自动关闭
+                anim: 2,
+                content: ['test/guodu.html', 'no'], //iframe的url，no代表不显示滚动条
+                end: function(){ //此处用于演示
+                    layer.open({
+                        type: 2,
+                        title: '很多时候，我们想最大化看，比如像这个页面。',
+                        shadeClose: true,
+                        shade: false,
+                        maxmin: true, //开启最大化最小化按钮
+                        area: ['893px', '600px'],
+                        content: '//fly.layui.com/'
+                    });
+                }
+            });
+        });
+
         //普通图片上传
         var uploadInst = upload.render({
             elem: '#uploadPic'
